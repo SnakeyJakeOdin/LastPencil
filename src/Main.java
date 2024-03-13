52c321fe8be7060faf6bf4c4c5f3f8df08631ff6
SnakeyJakeOdin <160692097+SnakeyJakeOdin@users.noreply.github.com>
2024-03-13 12:12:31 +0900

    Create getWhoGoesFirst method.
    This method asks the client to choose between two names in order to decide who goes first in the game. The method returns a string if none of the two names were chosen.

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,30 @@ public class Main {
 
     public static void main(String[] args) {
         int numPencils = getNumPencils();
+        String whoGoesFirst = getWhoGoesFirst();
         System.out.println(numPencils);
+        System.out.println(whoGoesFirst);
     }
 
     public static int getNumPencils() {
         System.out.println("How many pencils would you like to use:");
         return scanner.nextInt();
+    }
+
+    public static String getWhoGoesFirst() {
+        String name1 = "John";
+        String name2 = "Niko";
+        System.out.printf("Who will be first (%s, %s):%n", name1, name2);
+
+        String userChoice = scanner.next();
+        if (userChoice.equalsIgnoreCase(name1)) {
+            return name1;
+        }
+        else if (userChoice.equalsIgnoreCase(name2)) {
+            return name2;
+        }
+        else {
+            return "\"ERROR: No valid name was chosen\"";
+        }
     }
 }
