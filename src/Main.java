d82a5bf812d91081d44a6e6dd094c8b49748be30
SnakeyJakeOdin <160692097+SnakeyJakeOdin@users.noreply.github.com>
2024-03-14 11:03:32 +0900

    Start from scratch
    Now that I have to implement arrays into my program I am finding difficulty in rewriting my old code. I solve this I commented out my old code so that I can start from scratch while also picking out bits that I like from my old code.

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,31 @@ import java.util.Scanner;
 
 public class Main {
     public static Scanner scanner = new Scanner(System.in);
-    public static char[] stickArray;
+    // public static char[] stickArray;
 
     public static void main(String[] args) {
-        int n = getPencilNumber();
-        System.out.println(createString(n));
+        // get number of pencils
+        System.out.println("How many pencils would you like to use:");
+        int numPencils = scanner.nextInt();
+
+        // get first player
+        String[] players = new String[]{"John", "Jack"};
+        System.out.printf("Who will be the first (%s, %s):%n", players[0], players[1]);
+        String player = scanner.next();
+
+        // display information
+        char[] stickArray = new char[numPencils];
+        Arrays.fill(stickArray, '|');
+        System.out.println(stickArray);
     }
 
-    public static int getPencilNumber() {
+    /*
+    public static int getPencilCount() {
         System.out.println("How many pencils would you like to use:");
         return scanner.nextInt();
     }
 
-    public static String createString(int n) {
+    public static String createPencilString(int n) {
         stickArray = new char[n];
         Arrays.fill(stickArray, '|');
         return String.valueOf(stickArray);
@@ -37,5 +49,10 @@ public class Main {
             return "\"ERROR: No valid name was chosen\"";
         }
     }
+
+    public static void displayStartingState() {
+        System.out.printf("%s%n%s's turn", createPencilString(getPencilCount()), getWhoGoesFirst());
+    }
+     */
 
 }
